0b2f22f38bf1b8e2d2e5b43e22de4f04fba9684d
sbx_user1051 <sbx_user1051@169.254.8.173>
2022-07-17 14:20:14 +0000

    Sun Jul 17 14:20:14 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -13,10 +13,15 @@ def checkout(skus):
     if (sumItems != len(skus_list)):
         return -1
     else:
-        B = B - (E//2)
-        F = F - (F//3)
-        if(B<0):
-            B = 0
+        sumTotal = 0
+        for item, count in item_count:
+            print(item," ", count)
         
-        return (((A//5)*200) + ((A%5)//3) * 130 + ((A%5)%3) * 50)+ ((B//2)*45+(B%2)*30) + C*20 + D*15 + E*40 + F * 10
+        # B = B - (E//2)
+        # F = F - (F//3)
+        # if(B<0):
+        #     B = 0
+        
+        # return (((A//5)*200) + ((A%5)//3) * 130 + ((A%5)%3) * 50)+ ((B//2)*45+(B%2)*30) + C*20 + D*15 + E*40 + F * 10
     
+checkout("ABDDEGREGHTHKYUGJHREYHYUK")
