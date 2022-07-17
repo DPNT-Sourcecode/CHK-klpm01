402b5446d56df6e593976ab7c0fd9555a2c3161b
sbx_user1051 <sbx_user1051@169.254.8.173>
2022-07-17 13:50:53 +0000

    Sun Jul 17 13:50:53 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -9,9 +9,11 @@ def checkout(skus):
     B = skus_list.count('B')
     C = skus_list.count('C')
     D = skus_list.count('D')
+    E = skus_list.count('E')
     if (A+B+C+D != len(skus_list)):
         return -1
     else:
         # return ((A//3)*130+(A%3)*50)+((B//2)*45+(B%2)*30)+C*20+D*15
         
     
+
