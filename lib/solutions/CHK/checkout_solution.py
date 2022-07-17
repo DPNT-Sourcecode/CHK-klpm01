be054816b24424d95eedfb96b965dd45efecea18
sbx_user1051 <sbx_user1051@169.254.8.173>
2022-07-17 13:44:43 +0000

    Sun Jul 17 13:44:43 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,11 +3,7 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    # Items = {'A':0 ,'B':0,'C':0,'D':0}
-    # for item in skus:
-    #     if i == 'A':
-    #         Items['A']
-    skus = skus.upper()
+    
     skus_list = list(skus)
     A = skus_list.count('A')
     B = skus_list.count('B')
@@ -16,6 +12,5 @@ def checkout(skus):
     if (A+B+C+D != len(skus_list)):
         return -1
     else:
-        return ((A//3)*130+(A%3)*50)+((B//2)*45+(A%2)*30)+C*20+D*15
-    
+        return ((A//3)*130+(A%3)*50)+((B//2)*45+(B%2)*30)+C*20+D*15
     
